e8832fd5e85e7d7dc99cfeab24f4ad8d471acf07
Nico-Navigare <garciavilanicolas@gmail.com>
2026-03-13 20:29:47 -0300

    proyecto listo para deploy en Railway

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
 
-export const prisma = global.prisma || new PrismaClient({ log: ["error"] });
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
